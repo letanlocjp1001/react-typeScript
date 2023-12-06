b6671879bc1fbe0885d4812aff58d4f5b47266e5
letanloc <letanlocjp1001@gmail.com>
2023-12-06 18:59:53 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import CourseGoal from "./components/CourseGoal"
+
+function App() {
+
+  return (
+    <main>
+      <CourseGoal 
+      title="Learn React + ABC" 
+      description="Learn it from the ground up"/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,16 @@
+interface CourseGoalProps {
+    title: string; description: string
+}
+ 
+function CourseGoal({title, description}: CourseGoalProps){
+
+    return <article>
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+        <button>DELETE</button>
+    </article>
+}
+
+export default CourseGoal
